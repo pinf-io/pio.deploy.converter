115c2285578b104325c5fff174b9143befb290ce
Christoph Dorn <christoph@christophdorn.com>
2014-11-11 15:43:29 -0800

    link bins

--- nodejs-lib/tpl/scripts/build.sh
+++ nodejs-lib/tpl/scripts/build.sh
@@ -7,16 +7,50 @@ const FS = require("fs");
 var config = JSON.parse(FS.readFileSync(process.env.PIO_CONFIG_PATH));
 var descriptor = config.config["pio.service"].sourceDescriptor;
 if (!descriptor.dependencies) process.exit(0);
+
+function linkCommands(alias) {
+    var descriptorPath = PATH.join(process.env.PIO_SERVICE_PATH, "build/node_modules", alias, "package.json");
+    var binBasePath = PATH.join(process.env.PIO_SERVICE_PATH, "build/node_modules/.bin");
+    var descriptor = JSON.parse(FS.readFileSync(descriptorPath));
+    if (!descriptor.bin) {
+        return;
+    }
+    for (var command in descriptor.bin) {
+
+        var linkPath = PATH.join(binBasePath, command);
+        var fromPath = PATH.join(descriptorPath, "..", descriptor.bin[command]);
+
+        console.log("Linking '" + fromPath + "' to '" + linkPath + "'.");
+
+        if (!FS.existsSync(linkPath)) {
+            if (!FS.existsSync(fromPath)) {
+                return callback(new Error("Command declared as '" + command + "' in '" + descriptorPath + "' not found at '" + fromPath + "'!"));
+            }
+            if (!FS.existsSync(PATH.dirname(linkPath))) {
+                FS.mkdirSync(PATH.dirname(linkPath));
+            } else {
+                // Replace symlink if it exists.
+                try {
+                    FS.unlinkSync(linkPath);
+                } catch(err) {}
+            }
+            FS.symlinkSync(PATH.relative(PATH.dirname(linkPath), fromPath), linkPath);
+        }
+    }
+}
+
 for (var alias in descriptor.dependencies) {
 	var liveServicePath = PATH.join(config.config["pio.vm"].prefixPath, "services", alias, "live/install");
 	if (FS.existsSync(liveServicePath)) {
 		var linkPath = PATH.join(process.env.PIO_SERVICE_PATH, "build/node_modules", alias);
+		// Delete symlink which may be pointing to wrong directory and will give false for FS.exists.
 		try { FS.unlinkSync(linkPath); } catch(err) {}
 		console.log("Linking npm package " + liveServicePath + " to " + linkPath);
 		if (!FS.existsSync(PATH.dirname(linkPath))) {
 			FS.mkdirSync(PATH.dirname(linkPath));
 		}
 		FS.symlinkSync(liveServicePath, linkPath);
+		linkCommands(alias);
 	}
 }
 '
